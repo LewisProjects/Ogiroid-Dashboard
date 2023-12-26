5b3ee0827d16111690ec5bd5ffd0fe4f5b151f86
LevaniVashadze <vashadzelevani11@gmail.com>
2023-12-26 17:16:22 +0400

    reformat

--- index.py
+++ index.py
@@ -33,6 +33,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     avatar_url = db.Column(db.String(200))
 
+
 @app.route("/")
 def home() -> str:
     return render_template("index.html")
